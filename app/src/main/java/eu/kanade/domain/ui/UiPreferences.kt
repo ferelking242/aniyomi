7058c49a49225d17146e0024b26b08ea46a13a99
FERELKING242 <111065891+ferelking242@users.noreply.github.com>
2026-04-10 12:36:50 +0100

    feat: add dockStyle preference

--- app/src/main/java/eu/kanade/domain/ui/UiPreferences.kt
+++ app/src/main/java/eu/kanade/domain/ui/UiPreferences.kt
@@ -1,49 +1,53 @@
 package eu.kanade.domain.ui
 
-import eu.kanade.domain.ui.model.AppTheme
-import eu.kanade.domain.ui.model.NavStyle
-import eu.kanade.domain.ui.model.StartScreen
-import eu.kanade.domain.ui.model.TabletUiMode
-import eu.kanade.domain.ui.model.ThemeMode
-import eu.kanade.tachiyomi.util.system.DeviceUtil
-import eu.kanade.tachiyomi.util.system.isDynamicColorAvailable
-import tachiyomi.core.common.preference.PreferenceStore
-import tachiyomi.core.common.preference.getEnum
-import java.time.format.DateTimeFormatter
-import java.time.format.FormatStyle
-import java.util.Locale
+  import eu.kanade.domain.ui.model.AppTheme
+  import eu.kanade.domain.ui.model.DockStyle
+  import eu.kanade.domain.ui.model.NavStyle
+  import eu.kanade.domain.ui.model.StartScreen
+  import eu.kanade.domain.ui.model.TabletUiMode
+  import eu.kanade.domain.ui.model.ThemeMode
+  import eu.kanade.tachiyomi.util.system.DeviceUtil
+  import eu.kanade.tachiyomi.util.system.isDynamicColorAvailable
+  import tachiyomi.core.common.preference.PreferenceStore
+  import tachiyomi.core.common.preference.getEnum
+  import java.time.format.DateTimeFormatter
+  import java.time.format.FormatStyle
+  import java.util.Locale
 
-class UiPreferences(
-    private val preferenceStore: PreferenceStore,
-) {
+  class UiPreferences(
+      private val preferenceStore: PreferenceStore,
+  ) {
 
-    fun themeMode() = preferenceStore.getEnum("pref_theme_mode_key", ThemeMode.SYSTEM)
+      fun themeMode() = preferenceStore.getEnum("pref_theme_mode_key", ThemeMode.SYSTEM)
 
-    fun appTheme() = preferenceStore.getEnum(
-        "pref_app_theme",
-        if (DeviceUtil.isDynamicColorAvailable) {
-            AppTheme.MONET
-        } else {
-            AppTheme.DEFAULT
-        },
-    )
+      fun appTheme() = preferenceStore.getEnum(
+          "pref_app_theme",
+          if (DeviceUtil.isDynamicColorAvailable) {
+              AppTheme.MONET
+          } else {
+              AppTheme.DEFAULT
+          },
+      )
 
-    fun themeDarkAmoled() = preferenceStore.getBoolean("pref_theme_dark_amoled_key", false)
+      fun themeDarkAmoled() = preferenceStore.getBoolean("pref_theme_dark_amoled_key", false)
 
-    fun relativeTime() = preferenceStore.getBoolean("relative_time_v2", true)
+      fun relativeTime() = preferenceStore.getBoolean("relative_time_v2", true)
 
-    fun dateFormat() = preferenceStore.getString("app_date_format", "")
+      fun dateFormat() = preferenceStore.getString("app_date_format", "")
 
-    fun tabletUiMode() = preferenceStore.getEnum("tablet_ui_mode", TabletUiMode.AUTOMATIC)
+      fun tabletUiMode() = preferenceStore.getEnum("tablet_ui_mode", TabletUiMode.AUTOMATIC)
 
-    fun startScreen() = preferenceStore.getEnum("start_screen", StartScreen.ANIME)
+      fun startScreen() = preferenceStore.getEnum("start_screen", StartScreen.ANIME)
 
-    fun navStyle() = preferenceStore.getEnum("bottom_rail_nav_style", NavStyle.MOVE_HISTORY_TO_MORE)
+      fun navStyle() = preferenceStore.getEnum("bottom_rail_nav_style", NavStyle.MOVE_HISTORY_TO_MORE)
 
-    companion object {
-        fun dateFormat(format: String): DateTimeFormatter = when (format) {
-            "" -> DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT)
-            else -> DateTimeFormatter.ofPattern(format, Locale.getDefault())
-        }
-    }
-}
+      fun dockStyle() = preferenceStore.getEnum("dock_style", DockStyle.NORMAL)
+
+      companion object {
+          fun dateFormat(format: String): DateTimeFormatter = when (format) {
+              "" -> DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT)
+              else -> DateTimeFormatter.ofPattern(format, Locale.getDefault())
+          }
+      }
+  }
+  
